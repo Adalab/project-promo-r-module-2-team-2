e83fbccc2664ed374a574c23f7d81504f3d84420
noemiyuen <noemiyuen@gmail.com>
2022-11-08 17:04:01 +0100

    clean code

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,42 @@
-"use strict";
+/* eslint-disable indent */
+'use strict';
 
-const legendElement = document.querySelectorAll(".js-containerLegend");
+//querySelectors
 
+const legendElement = document.querySelectorAll('.js-containerLegend');
+const upArrow = document.querySelector('.js-upArrow');
+const containerDesign = document.querySelector('.js-palettes');
+const upArrow1 = document.querySelector('.js-upArrow1');
+const containerFill = document.querySelector('.js-form__contact');
+const upArrow2 = document.querySelector('.js-upArrow2');
+const containerCreate = document.querySelector('.js-containerCreate');
+const btnCreate = document.querySelector('.js-btn-create');
+const shareTwitter = document.querySelector('.js-shareTwitter');
+const resetBtnElement = document.querySelector('.js-resetBtn');
 
-const upArrow = document.querySelector(".js-upArrow");
-const containerDesign = document.querySelector(".js-palettes");
-const upArrow1 = document.querySelector(".js-upArrow1");
-const containerFill = document.querySelector(".js-form__contact");
-const upArrow2 = document.querySelector(".js-upArrow2");
-const containerCreate = document.querySelector(".js-containerCreate");
+const careerInput = document.querySelector('.js-career');
+const nameInput = document.querySelector('.js-name');
+const careerCardInput = document.querySelector('.js-careerCard');
+const nameCardInput = document.querySelector('.js-nameCard');
 
-const btnCreate = document.querySelector(".js-btn-create");
-const shareTwitter = document.querySelector(".js-shareTwitter");
+const mailInput = document.querySelector('.js-mailInput');
+const telInput = document.querySelector('.js-telInput');
+const linkedinInput = document.querySelector('.js-linkedinInput');
+const githubInput = document.querySelector('.js-githubInput');
+const telCard = document.querySelector('.js-telCard');
+const mailCard = document.querySelector('.js-mailCard');
+const linkedinCard = document.querySelector('.js-linkedinCard');
+const githubCard = document.querySelector('.js-githubCard');
 
-const resetBtnElement = document.querySelector(".js-resetBtn");
+const fieldsetFill = document.querySelector('.js-fieldsetFill');
 
+const cardLinkElement = document.querySelector('.js-cardLink ');
+const twitterBtn = document.querySelector('.js-twitterBtn');
 
-
-
-
+const paletteOption1 = document.querySelector('.js-palette1');
+const paletteOption2 = document.querySelector('.js-palette2');
+const paletteOption3 = document.querySelector('.js-palette3');
+const cardDisplayContainer = document.querySelector('.js-cardDisplay__card');
 
 
 
@@ -26,16 +44,16 @@ const resetBtnElement = document.querySelector(".js-resetBtn");
 
 function addClass (element, classElement) {
   element.classList.add(classElement);
-}
+  }
 function removeClass (element, classElement) {
   element.classList.remove(classElement);
-}
+  }
 function toggleClass (element, classElement) {
   element.classList.toggle(classElement);
-}
+  }
 
 
-//mostrar section abierta ocultar el resto
+//SECCIONES: mostrar y cerrar secciones no seleccionadas
 
 function colapse(event) {
   const clickElement = event.currentTarget;
@@ -72,14 +90,14 @@ function colapse(event) {
 function toggleLegend() {
   for (let i = 0; i < legendElement.length; i++) {
     legendElement[i].addEventListener("click", colapse);
+    }
   }
-}
 toggleLegend();
 
 
-//boton CREAR TARJETA
+//BOTON CREAR: mostrar Twitter
 
-btnCreate.addEventListener("click", (event) => {
+btnCreate.addEventListener('click', (event) => {
   event.preventDefault();
   removeClass (shareTwitter,'hide');
   addClass (btnCreate,'btnGrey');
@@ -87,29 +105,7 @@ btnCreate.addEventListener("click", (event) => {
 
 
 
-
 // VOLCADO DE DATOS A LA TARJETA
-
-const careerInput = document.querySelector(".js-career");
-const nameInput = document.querySelector(".js-name");
-const careerCardInput = document.querySelector(".js-careerCard");
-const nameCardInput = document.querySelector(".js-nameCard");
-
-const mailInput = document.querySelector(".js-mailInput");
-const telInput = document.querySelector(".js-telInput");
-const linkedinInput = document.querySelector(".js-linkedinInput");
-const githubInput = document.querySelector(".js-githubInput");
-const telCard = document.querySelector(".js-telCard");
-const mailCard = document.querySelector(".js-mailCard");
-const linkedinCard = document.querySelector(".js-linkedinCard");
-const githubCard = document.querySelector(".js-githubCard");
-
-const fieldsetFill = document.querySelector(".js-fieldsetFill");
-
-const cardLinkElement = document.querySelector('.js-cardLink ');
-const twitterBtn = document.querySelector('.js-twitterBtn');
-
-
 
 let data = {
   palette: "",
@@ -128,13 +124,13 @@ function dataCollect() {
   const inputTarget = event.target;
   let inputTargetValue = inputTarget.value;
   data[inputTarget.name] = inputTargetValue;
- /*  console.log(data); */
   //si no se elige paleta, se selecciona la predefinida
   if (data.palette === '') {
     data.palette = 'palette1';
-  }
+    }
   }
 
+//función pintar
 function renderCard() {
   nameCardInput.innerHTML = data.name;
   careerCardInput.innerHTML = data.job;
@@ -150,13 +146,29 @@ function renderCard() {
   }
 }
 
-fieldsetFill.addEventListener("input", (event) => {
+//guardar los datos en local para cuando se cargue la pagina
+
+/* function saveLocalStorage(data){
+  localStorage.setItem('dataFromForm', JSON.stringify(data));
+}
+
+data=localStorage.getItem('dataFromForm');
+ */
+/* updatePreview();
+nameInput.value = data.name;
+jobInput.value = data.job; */
+
+
+//recoger info de los inputs
+fieldsetFill.addEventListener('input', (event) => {
   event.preventDefault();
   dataCollect();
   renderCard();
+  //updatePreview();
+/*   saveLocalStorage(data); */
 });
 
-containerDesign.addEventListener("input", (event) => {
+containerDesign.addEventListener('input', (event) => {
   event.preventDefault();
   dataCollect();
 });
@@ -177,49 +189,41 @@ function handleReset(event) {
   data.github= '';
   data.photo= '';
 
-
-  nameInput.value = "";
-  careerInput.value = "";
-  mailInput.value = "";
-  telInput.value = "";
-  linkedinInput.value = "";
-  githubInput.value = "";
-  nameCardInput.innerHTML = "Nombre Apellido";
-  careerCardInput.innerHTML = "Front-end developer";
-  telCard.href = "tel:example";
-  mailCard.href = "mailto:example@gmail.com";
-  linkedinCard.href = "https://www.linkedin.com";
-  githubCard.href = "https://github.com/";
+  nameInput.value = '';
+  careerInput.value = '';
+  mailInput.value = '';
+  telInput.value = '';
+  linkedinInput.value = '';
+  githubInput.value = '';
+  nameCardInput.innerHTML = 'Nombre Apellido';
+  careerCardInput.innerHTML = 'Front-end developer';
+  telCard.href = 'tel:example';
+  mailCard.href = 'mailto:example@gmail.com';
+  linkedinCard.href = 'https://www.linkedin.com';
+  githubCard.href = 'https://github.com/';
 }
 
-resetBtnElement.addEventListener("click", handleReset);
+resetBtnElement.addEventListener('click', handleReset);
 
 
 //PALETAS - volcado
-
-const paletteOption1 = document.querySelector('.js-palette1');
-const paletteOption2 = document.querySelector('.js-palette2');
-const paletteOption3 = document.querySelector('.js-palette3');
-const cardDisplayContainer = document.querySelector('.js-cardDisplay__card');
-
-
 const changePalette = (event) => {
   cardDisplayContainer.classList.remove ('palette1');
-  cardDisplayContainer.classList.remove ('palette2');  
-  cardDisplayContainer.classList.remove ('palette3');  
+  cardDisplayContainer.classList.remove ('palette2');
+  cardDisplayContainer.classList.remove ('palette3');
   cardDisplayContainer.classList.add(event.currentTarget.value);
-};
+  };
 
 paletteOption1.addEventListener ('click', changePalette);
 paletteOption2.addEventListener ('click', changePalette);
 paletteOption3.addEventListener ('click', changePalette); 
 
 
-//guardar los datos en local para cuando se cargue la pagina
 
-function saveLocalStorage (data){
-  localStorage.setItem('dataFromForm', JSON.stringify(data));
-}
+/*   localStorage.setItem('dataFromForm', JSON.stringify(data));
+  saveLocalStorage ('dataFromForm');
+  console.log('dataFromForm');
+ */
 
 
 
@@ -227,10 +231,7 @@ function saveLocalStorage (data){
 
 btnCreate.addEventListener('click', (event)=> {
   event.preventDefault();
-/*   localStorage.setItem('dataFromForm', JSON.stringify(data));
-  saveLocalStorage ('dataFromForm');
-  console.log('dataFromForm');
- */
+  //enviar datos a API
   fetch ('https://awesome-profile-cards.herokuapp.com/card',
     {
       method:'POST',
@@ -248,14 +249,11 @@ btnCreate.addEventListener('click', (event)=> {
       const twitterHref = `https://twitter.com/intent/tweet?
       text=Aquí%20esta%20mi%20tarejeta%20de%20visita%20creada%20con%20Awesome%20profile%20cards&url=${responseURL}&hashtags=tarjetadevisita,awesomeprofilecards`;
       twitterBtn.href = twitterHref;
-
-
-    } else {
-      cardLinkElement.innerHTML = 'Rellena todos los campos';
-    }
-    })
-
-})
+      } else {
+        cardLinkElement.innerHTML = 'Rellena todos los campos';
+      }
+    });
+  });
 
 
 
@@ -266,7 +264,7 @@ btnCreate.addEventListener('click', (event)=> {
 
 /* data = JSON.parse(localStorage.getItem('dataFromForm')); */
 
-/* 
+/*
 function updatePreview(){
   nameInput.value = data.name;
   careerInput.value = data.job;
